f3e3ac2e5c1a18b540f7f05669792c139a2bf39b
Sundaram Saroj <sundaramsaroj@Sundarams-MacBook-Air.local>
2025-08-12 21:56:35 +0530

    started auth setup infrontend

---
+++ frontend/my-app/src/routes/login/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+    
+</script>
+<main>
+    <form
+  action="https://formspree.io/f/myzjdjbv"
+  method="POST"
+>
+  <label>
+    Your email:
+    <input type="email" name="email">
+  </label>
+  <label>
+    Your message:
+    <textarea name="message"></textarea>
+  <button type="submit">Send</button>
+</form>
+</main>
---
+++ frontend/my-app/src/routes/signup/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+
+</script>
+<main>
+    <form
+  action="https://formspree.io/f/myzjdjbv"
+  method="POST"
+>
+  <label>
+    Your email:
+    <input type="email" name="email">
+  </label>
+  <label>
+    Your Password:
+    <input type="password" name="password"/>
+  </label>
+  <button type="submit">Send</button>
+</form>
+</main>
