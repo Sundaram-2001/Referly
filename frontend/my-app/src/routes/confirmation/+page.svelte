c5c4ffa8e69ecfd3e87a5525fb6c701f5aceebf8
Sundaram Saroj <sundaramsaroj@Sundarams-MacBook-Air.local>
2025-08-19 20:39:06 +0530

    handled email confirmation

--- frontend/my-app/src/routes/confirmation/+page.svelte
+++ frontend/my-app/src/routes/confirmation/+page.svelte
@@ -1,3 +1,25 @@
+<script>
+    import { onMount } from "svelte";
+    import { supabase } from "$lib/supabaseClient";
+    import { goto } from "$app/navigation";
+    let message='confirming you email...'
+    onMount(async()=>{
+        const {data,error}=await supabase.auth.getSession()
+        if(error){
+            console.error(error)
+            message="error confirming your email , kindly signup again"
+            return
+        }
+        if(data.session){
+            message="Email , confirmed , redirecting to onboarding page.."
+            setTimeout(() => {
+        goto("/onboard")
+      }, 2000);
+        } else{
+            message = 'No active session found. Please log in.';
+        }
+    })
+</script>
 <main>
-    <p>Thanks for confirming your email! let us know more about you , <a href="onboard ">click here!</a></p>
+  <p>{message}</p>
 </main>
