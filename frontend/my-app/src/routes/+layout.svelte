2cf11ec3b62a2b00431788b850d868c9762668b2
Sundaram Saroj <sundaramsaroj@Sundarams-MacBook-Air.local>
2025-08-21 18:48:50 +0530

    adding clerk

--- frontend/my-app/src/routes/+layout.svelte
+++ frontend/my-app/src/routes/+layout.svelte
@@ -1,11 +1,12 @@
 <script>
 	import favicon from '$lib/assets/favicon.svg';
-
+    import { ClerkProvider } from 'svelte-clerk';
 	let { children } = $props();
+	const publishableKey=import.meta.env.PUBLIC_CLERK_PUBLISHABLE_KEY
 </script>
-
+<ClerkProvider publishablekey={publishableKey} children={children}/>
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-{@render children?.()}
+
--- frontend/my-app/src/routes/signup/+page.svelte
+++ frontend/my-app/src/routes/signup/+page.svelte
@@ -1,49 +1,6 @@
 <script>
-  import { supabase } from "$lib/supabaseClient";
-
-  let email = '';
-  let password = '';
-  let name = '';
-  let successMessage = '';
-  let errorMessage = '';
-
-  const signUpUser = async () => {
-    const { data, error } = await supabase.auth.signUp({
-      email,
-      password,
-      options: {
-        data: { name }, 
-        emailRedirectTo: `${import.meta.env.PUBLIC_SITE_URL}/confirmation`
-      }
-    });
-
-    if (error) {
-      console.error(error);
-      errorMessage = error.message;
-    } else {
-      successMessage = "Awesome, kindly check your email for next steps!";
-    }
-  };
+  import {SignUp} from "svelte-clerk"
 </script>
-
 <main>
-  <form on:submit|preventDefault={signUpUser}>
-    <label for="name">Your Name</label>
-    <input type="text" name="name" bind:value={name} required />
-
-    <label for="email">Your Email</label>
-    <input type="email" name="email" bind:value={email} required />
-
-    <label for="password">Your Password</label>
-    <input type="password" name="password" bind:value={password} required />
-
-    <button type="submit">Sign Up</button>
-
-    {#if errorMessage}
-      <p style="color: red;">{errorMessage}</p>
-    {/if}
-    {#if successMessage}
-      <p style="color: green;">{successMessage}</p>
-    {/if}
-  </form>
+    <SignUp />
 </main>
